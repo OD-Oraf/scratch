31d41624d5a93a78f9ad30c086568ff106d449d4
OD Oraf <poseidonog@gmail.com>
2024-12-12 21:39:15 -0500

    add Scratch.java

---
+++ java/Scratch.java
@@ -0,0 +1,8 @@
+import java.io.File;
+
+class Scratch {
+    public static void main(String[] args) {
+        File file = new File("categories.json");
+        System.out.println(file.toString());
+    }
+}
