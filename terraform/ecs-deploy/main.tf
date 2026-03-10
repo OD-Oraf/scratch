60ec9257b7590d70fed8ab9635c7ddfff1394585
OD Oraf <poseidonog@gmail.com>
2026-03-10 09:51:28 -0400

    update health check endpoint

--- terraform/ecs-deploy/main.tf
+++ terraform/ecs-deploy/main.tf
@@ -330,7 +330,7 @@ resource "aws_lb_target_group" "main" {
     unhealthy_threshold = 3
     timeout             = 10
     interval            = 30
-    path                = "/greeting"
+    path                = "/health"
     matcher             = "200"
   }
 
